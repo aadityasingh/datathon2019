134436fe869e5f1939e520eb6a8d266cf7caa247
Aaditya Singh <aaditya@mit.edu>
2019-02-09 12:47:33 -0500

    was working on plant gas model but pivoting.

--- plant_to_gas/main.py
+++ plant_to_gas/main.py
@@ -1,96 +1,73 @@
-import torch.backends.cudnn as cudnn
-import torch.nn.init as init
-import torch.utils.data
-import torchvision.utils as tvut
+# PLANT TO GAS NEURAL NETWORK MODEL HERE:
+from torch.autograd import Variable
+from tensorboardX import SummaryWriter
+import shutil
+from tqdm import tqdm
+import numpy as np
+import utils
+
+import os
+import pdb
+import pickle
 import argparse
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Torch imports
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
+import torch.optim as optim
 
-from model import VAE
-from data_loader import load_data
-from train import Trainer
-from loss import Loss
+# Numpy & Scipy imports
+import numpy as np
+import scipy
+import scipy.misc
 
-image_size = 128
+from datasets import PlantGasDataset
 
-data_load_params = {'batch_size' : 10, 'shuffle' : True, 'cuda': False, 'num_workers' : 1, 'pin_memory' : True,
-					'image_size': image_size, 'valid_split': 0.2, 'data_path': './data/touse/',
-					'normalize': [[0.57867116, 0.53378844, 0.47493997], [0.2312678, 0.21923426, 0.22560729]]}
+# Input plant types we care about (listed for easy tuning)
+sources = ['nuclear', 'wind', 'hydroelectric', 'natural gas', 'coal', 'pumped storage', 'biomass', 'solar', 'geothermal']
+# Feature gases that we care about
+gases = ['TSO4','TNH4','CA','MG','NA','K','CL','NSO4','NHNO3','WSO2','WNO3']
+gases.remove('MG')
+gases.remove('NA')
+gases.remove('K')
+gases.remove('CL')
 
-train_loader, test_loader = load_data("Pointillism", data_load_params)
-model = VAE()
-if torch.cuda.is_available():
-	model.cuda()
-	print('Using GPU')
+pgd = PlantGasDataset(plantCSV, gasCSV, sources, gases)
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type = int, default = 10000)
+    parser.add_argument('--test_every', type = int, default = 100)
+    parser.add_argument('--checkpoint', type=int, default=0)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--lr_step', type=int, default=1)
+    parser.add_argument('--hidden_dim', type=int, default=10)
+    parser.add_argument('--decay', type=float, default=0.995)
+
+    return parser
 
 def train(opts):
-	training_params = {'num_epochs' : opts.epochs, 'learning_rate' : opts.lr, 'weight_decay' : 0.3, 'learning_rate_decay' : opts.decay, 'cuda' : False, 
-		'summary_dir' : './runs/logs/', 'checkpoint_dir' : './runs/checkpoints/'}
+    training_params = {'num_epochs' : opts.epochs, 'learning_rate' : opts.lr, 'weight_decay' : 0.3, 
+        'learning_rate_decay' : opts.decay, 'learning_rate_step': opts.lr_step, 'cuda' : False, 
+        'summary_dir' : './runs/logs/', 'checkpoint_dir' : './runs/checkpoints/', 'hidden_dim': opts.hidden_dim}
 
-	if opts.checkpoint == 1:
-		checkpoint = torch.load('./runs/checkpoints/checkpoint2.pth.tar')
-		model.load_state_dict(checkpoint['state_dict'])
+    if opts.checkpoint == 1:
+        checkpoint = torch.load('./runs/checkpoints/checkpoint2.pth.tar')
+        model.load_state_dict(checkpoint['state_dict'])
 
-	loss = Loss()
+    loss = Loss()
 
-	trainer = Trainer(model, loss, train_loader, test_loader, training_params)
+    trainer = Trainer(model, loss, train_loader, test_loader, training_params)
 
-	# print(trainer)
+    # print(trainer)
 
-	trainer.train(opts)
-
-# CAUTION: Note that vae_params must be the same as the checkpoint... perhaps we can save this with the checkpoint for future
-def gen_image(checkpoint_file):
-	checkpoint = torch.load(checkpoint_file)
-
-	model.load_state_dict(checkpoint['state_dict'])
-	print(len(data.train_set))
-	# print(data.train_set[0][0])
-# <<<<<<< HEAD
-	print(data.train_set[0][0].size())
-	batch1 = data.train_set[0][0].unsqueeze(0)
-	print(batch1.size())
-	print(batch1)
-	to_save = data.un_norm(data.train_set[0][0])
-	tvut.save_image(to_save, "goal1.png")
-	model.eval()
-	mu, logvar = model.encode(batch1)
-	lat = model.reparamaterize(mu, logvar)
-	print(model.decode(mu)[0])
-	print(mu)
-	tvut.save_image(data.un_norm(model.decode(mu)[0]), "generated_image1.png")
-# =======
-# 	# print(data.train_set[0][0].size())
-# 	batch1 = data.train_set[9][0].unsqueeze(0).cuda()
-# 	# print(batch1.size())
-
-# 	inp = Variable(batch1)
-# 	tvut.save_image(batch1, "goal2.png")
-# 	# print(inp)
-# 	model.eval()
-# 	mu, logvar = model.encode(batch1)
-# 	lat = model.reparamaterize(mu, logvar)
-# >>>>>>> gans
-	
-	# generated = model.decode(mu)
-
-	# print(mu, logvar, generated)
-	# tvut.save_image(generated, "generated_image2.png")
-	
-def create_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--epochs', type = int, default = 10000)
-	parser.add_argument('--test_every', type = int, default = 100)
-	parser.add_argument('--checkpoint', type=int, default=0)
-	parser.add_argument('--lr', type=float, default=0.01)
-	parser.add_argument('--decay', type=float, default=0.995)
-	return parser
-
+    trainer.train(opts)
 
 if __name__ == "__main__":
-	parser = create_parser()
-	opts = parser.parse_args()
-	
-	train(opts)
-	# gen_image('./runs/checkpoints/checkpoint2.pth.tar')
+    parser = create_parser()
+    opts = parser.parse_args()
+    
+    train(opts)
